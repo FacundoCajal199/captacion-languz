bb8bcf38483921f30b09bc4a168d394ffa124181
Facund05 <118226317+FacundoCajal199@users.noreply.github.com>
2023-10-12 21:12:30 -0300

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import './App.css'
-import Navbar from './components/Navbar'
-import Titular from './components/Titular'
-import Boton from './components/Colttoaction'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Descripcion from './components/Descripcion';
-import Tarjeta from './components/Card';
-import { data } from './components/Card';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Video from "./components/Videos";
+import Titular from "./components/Titular";
+import Boton from "./components/Colttoaction";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Descripcion from "./components/Descripcion";
+import Tarjeta from "./components/Card";
+import { data } from "./components/Card";
 
 function App() {
   return (
     <>
-    <Navbar></Navbar>
-   <Titular></Titular>
-   <Boton></Boton>
-   <Descripcion></Descripcion>
-   <Tarjeta items={data} />
-   
+      <Navbar></Navbar>
+      <Titular></Titular>
+      <Video></Video>
+      <Boton></Boton>
+      <Descripcion></Descripcion>
+      <Tarjeta items={data} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Titular.jsx
+++ src/components/Titular.jsx
@@ -1,11 +1,11 @@
-import Video from "./video";
+
 const Titular=()=>{
     return(
     <>
 <div className="centrar-columnas">
 <h1 className="text-center container pt-4">Aprende como <b>2800 de nuestros estudiantes latinos</b>dominaron el ingles en 90 dias</h1>
     <p className="pt-3 text-center h3">Solo te falta <b>1 PASO </b> para asegurar tu cupo...</p>
-    <Video></Video>
+   
 </div>
 
     
