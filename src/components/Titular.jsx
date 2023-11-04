6f5071790c10ec3fff8442578fd0ed384435d03b
Facund05 <118226317+FacundoCajal199@users.noreply.github.com>
2023-11-04 15:01:06 -0300

    cambios en main

--- src/components/Titular.jsx
+++ src/components/Titular.jsx
@@ -3,7 +3,7 @@ const Titular=()=>{
     return(
     <>
 <div className="centrar-columnas">
-<h1 className="text-center container pt-4 style-titulos">Descubre como <b>2800 de nuestros estudiantes latinos</b> aprendieron ingles en 90 dias</h1>
+<h1 className="text-center container pt-4 ">Descubre como <b>2800 de nuestros estudiantes latinos</b> aprendieron ingles en 90 dias</h1>
     <p className="pt-3 text-center h3 container">Solo te falta <b>1 PASO </b> para asegurar tu cupo...</p>
    
 </div>
