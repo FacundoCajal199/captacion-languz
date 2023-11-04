5e9a2db3b9a383aaa9f955551ee34d2d46631342
Facund05 <118226317+FacundoCajal199@users.noreply.github.com>
2023-11-04 15:11:57 -0300

    cambios en main

--- src/components/Descripcion.jsx
+++ src/components/Descripcion.jsx
@@ -6,7 +6,7 @@ const Descripcion = () => {
       <p className="text-center h4 pt-2 ">
       te avisaremos en tiempo real cuando se estrenen
         cada una de las clases en vivo para que ingreses y no te pierdas ninguna <br/><br/>
-        Por tal motivo hemos creado un grupo de WhatsApp, en donde te enviaremos el link de cada entrenamiento unos minutos antes de su estreno, además contenido e información muy importante, en este grupo solo escribirá el administrador y no vas a recibir spam.
+        Por tal motivo hemos creado un grupo de WhatsApp, en donde te enviaremos el link de cada entrenamiento unos minutos antes de su estreno, además contenido e información muy importante
         <br />
         <br />
         Para no perderte ninguna de nuestras clases, solo deberás dar clic en el botón verde y unirte al grupo de WhatsApp.
