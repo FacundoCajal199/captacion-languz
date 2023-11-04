003f0f596168e7e4240d1a54c860ff2cdcef955d
Facund05 <118226317+FacundoCajal199@users.noreply.github.com>
2023-11-03 21:05:57 -0300

    cambios en main

--- src/components/Descripcion.jsx
+++ src/components/Descripcion.jsx
@@ -2,10 +2,10 @@ const Descripcion = () => {
   return (
     <>
     <div className="container py-5 ">
-    <h2 className="text-center h1">EVENTO DISPONIBLE POR ÚNICA VEZ DEL 16 AL 20 DE OCTUBRE</h2>
+    <h2 className="text-center h1">EVENTO DISPONIBLE POR ÚNICA VEZ</h2>
       <p className="text-center h4 pt-2 ">
-      Tenemos un gran desafío y es avisarte en tiempo real cuando se estrenen
-        cada uno de los enlaces de las clases en vivo para que ingreses y no te pierdas ninguna <br/><br/>
+      te avisaremos en tiempo real cuando se estrenen
+        cada una de las clases en vivo para que ingreses y no te pierdas ninguna <br/><br/>
         Por tal motivo hemos creado un grupo de WhatsApp, en donde te enviaremos el link de cada entrenamiento unos minutos antes de su estreno, además contenido e información muy importante, en este grupo solo escribirá el administrador y no vas a recibir spam.
         <br />
         <br />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ const Navbar =()=>{
 return(
 <>
 <div className="bg-rosa centrar">
-<h2 className="text-center">EVENTO DISPONIBLE POR UNICA VES DEL 6 AL 10 DE MARZO</h2>
+<h2 className="text-center">EVENTO DISPONIBLE POR UNICA </h2>
 </div>
 </>
 );
